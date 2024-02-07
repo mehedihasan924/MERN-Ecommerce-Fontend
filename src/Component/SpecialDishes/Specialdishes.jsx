9ee16a39e2a5de7ff7f48325ee01c9bf6b179365
mehedihasan924 <mdhasanmehedi924@gmail.com>
2024-02-08 00:25:54 +0600

    added React Slick

--- src/Component/SpecialDishes/Specialdishes.jsx
+++ src/Component/SpecialDishes/Specialdishes.jsx
@@ -1,12 +1,76 @@
 import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Specialdishes = () => {
+    const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
       <div className='max-w-screen-2xl lg:px-24 container my-20'>
           <div className='text-left'>
               <p className='text-red text-lg font-semibold uppercase tracking-wide'>special Dishes </p>
-              <h2 className='text-4xl md:text-5xl  font-bold my-2 md:leanding-snug leading-snug'> Standout Dishes <br /> From Our Menu</h2>
+              <h2 className='text-4xl md:text-5xl md:w-[520px] font-bold my-2 md:leanding-snug leading-snug'> Standout Dishes  From Our Menu</h2>
           </div>
+        {/* Slider React Slick */}
+         <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+        <div>
+          <h3>7</h3>
+        </div>
+        <div>
+          <h3>8</h3>
+        </div>
+      </Slider>
       </div>
   )
 }
